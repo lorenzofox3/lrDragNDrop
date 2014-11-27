539449e5bf7b61a2043bfb82372c28cd1b3bbc37
lorenzofox3 <laurent34azerty@gmail.com>
2014-11-27 09:54:28 +0700

    set up karma conf for CI

--- karma.conf.js
+++ karma.conf.js
@@ -60,7 +60,7 @@ module.exports = function (config) {
         // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
         // - PhantomJS
         // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'],
 
 
         // If browser does not capture in given timeout [ms], kill it
@@ -69,6 +69,6 @@ module.exports = function (config) {
 
         // Continuous Integration mode
         // if true, it capture browsers, run tests and exit
-        singleRun: false
+        singleRun: true
     });
 };
